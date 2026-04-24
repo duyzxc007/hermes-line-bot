741e4d2e550d2eb4566b41d0b6ee4c8e852d6796
User <user@example.com>
2026-04-24 14:13:12 +0700

    Revert back to local setup without Replit settings

--- main.py
+++ main.py
@@ -283,5 +283,4 @@ def handle_location_message(event):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.environ.get("PORT", 5000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
